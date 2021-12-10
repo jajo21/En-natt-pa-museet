8ec3f3d322461df4c57a5db9bd39958e2b8f6f3c
jajo21 <89775394+jajo21@users.noreply.github.com>
2021-12-10 10:49:24 +0100

    Flyttade en boolvariabel i VirtualMuseum och formaterade MuseumCollection

--- museet/Models/MuseumCollection.cs
+++ museet/Models/MuseumCollection.cs
@@ -4,11 +4,12 @@ namespace Museet.Models
 {
     // MuseumCollection är en klass som tar hand om lagringen av nyskapade museum i en dictionary och dess nödvändiga metoder.
     public class MuseumCollection
-	{
+    {
         Dictionary<string, Museum> museumDictionary;
         string visiting;
 
-        public MuseumCollection() {
+        public MuseumCollection()
+        {
             museumDictionary = new Dictionary<string, Museum>();
             this.visiting = "";
         }
@@ -16,38 +17,43 @@ namespace Museet.Models
         {
             string museumName = museum.GetMuseumName();
             museumDictionary.Add(museumName, museum);
-        } 
+        }
         public string GetAllMuseumNames()
         {
             string museumName = "";
-            foreach(var museum in museumDictionary)
+            foreach (var museum in museumDictionary)
             {
-                if(visiting == museum.Key) {
+                if (visiting == museum.Key)
+                {
                     museumName += $"{museum.Key} [Du är här]\n";
                 }
-                else {
-                   museumName += $"{museum.Key}\n"; 
+                else
+                {
+                    museumName += $"{museum.Key}\n";
                 }
             }
             return museumName;
         }
-        public void SetVisitingMuseum(string input) {
-            foreach(var museum in museumDictionary)
+        public void SetVisitingMuseum(string input)
+        {
+            foreach (var museum in museumDictionary)
             {
-              if(museum.Key == input) 
-              {
-                  this.visiting = input;
-              }
+                if (museum.Key == input)
+                {
+                    this.visiting = input;
+                }
             }
         }
-        public string GetVisitingMuseumName(){
+        public string GetVisitingMuseumName()
+        {
             return this.visiting;
         }
-        public Dictionary<string, Museum> GetDictionary () 
+        public Dictionary<string, Museum> GetDictionary()
         {
             return museumDictionary;
         }
-        public int GetDictionaryCount() {
+        public int GetDictionaryCount()
+        {
             return museumDictionary.Count;
         }
     }
